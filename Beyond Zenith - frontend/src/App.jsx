cc347c35a175be0a4c197b3b223f21efaac6ec45
ChandiraVihan <chandiravihan@gmail.com>
2025-12-28 17:25:22 +0530

    Portal

--- Beyond Zenith - frontend/src/App.jsx
+++ Beyond Zenith - frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Header.jsx'
+import Portal from './Portal.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Portal />
     </>
   )
 }
---
+++ Beyond Zenith - frontend/src/Portal.jsx
@@ -0,0 +1,10 @@
+import './Portal.css'
+
+function portal() {
+  return (
+    <div className="portal">
+    </div>
+  );
+}
+
+export default portal;
