a1654c90de0918149bfc27a8ab539a14d60b3f09
ChandiraVihan <chandiravihan@gmail.com>
2025-12-29 11:46:22 +0530

    nav-bar-ini

--- Beyond Zenith - frontend/src/Header.jsx
+++ Beyond Zenith - frontend/src/Header.jsx
@@ -5,7 +5,12 @@ function header(){
     return (
         <>
         <header>
+            <nav> 
             <h1>Beyond Zenith</h1>
+            <div>
+            <button>LOGIN {'>'}</button>
+            </div>
+            </nav>
         </header>
         </>
                 )
