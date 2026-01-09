fd795b1ef60c401b8233addf93d53663e2beb794
ChandiraVihan <chandiravihan@gmail.com>
2026-01-09 23:36:54 +0530

    registration

--- Beyond Zenith - frontend/src/Register.jsx
+++ Beyond Zenith - frontend/src/Register.jsx
@@ -6,7 +6,20 @@ function Register(){
   return (
     <main className="register-container">
       <div className="register-card">
-        <h1>Sign Up</h1>
+      
+        <h1>Registration</h1>
+        
+        <div className="form-wrapper">
+          <form>
+            <div className="input-group-reg">
+               <span className="mail-icon">
+              <ion-icon name="mail"></ion-icon>
+            </span>
+          <input type="email" required></input>
+          <label>Email :</label>
+              </div>
+          </form>
+        </div>
       </div>
     </main>
   )
