92a3a6b1dd8871f273e44e8b9e682f94920cbd96
ChandiraVihan <chandiravihan@gmail.com>
2026-01-11 22:26:20 +0530

    reg-layout-done

--- Beyond Zenith - frontend/src/Register.jsx
+++ Beyond Zenith - frontend/src/Register.jsx
@@ -11,12 +11,20 @@ function Register(){
         
         <div className="form-wrapper">
           <form>
+            <div className="reg-grid">
             <div className="reg-username">
                <span className="reg-mail-icon">
               <ion-icon name="person-add"></ion-icon>
             </span>
           <input type="text" required></input>
           <label>Username :</label>
+          </div>
+           <div className="reg-pass">
+               <span className="reg-pass-icon">
+              <ion-icon name="lock"></ion-icon>
+            </span>
+          <input type="password" required></input>
+          <label>Password :</label>
           </div>
             <div className="reg-email">
                <span className="reg-mail-icon">
@@ -25,19 +33,13 @@ function Register(){
           <input type="email" required></input>
           <label>Email :</label>
               </div>
-              <div className="reg-pass">
-               <span className="reg-pass-icon">
-              <ion-icon name="lock"></ion-icon>
-            </span>
-          <input type="password" required></input>
-          <label>Password :</label>
-          </div>
               <div className="reg-confirm-pass">
                <span className="reg-passconf-icon">
               <ion-icon name="checkmark-circle"></ion-icon>
             </span>
           <input type="password" required></input>
           <label>Confirm Password :</label>
+          </div>
           </div>
           <div className="register-button">
             <button type="submit">Register</button>
