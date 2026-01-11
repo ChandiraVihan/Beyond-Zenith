ef3daaeed29471c8ebbe391afd5d6f477a20cd1e
ChandiraVihan <chandiravihan@gmail.com>
2026-01-11 20:17:14 +0530

    initial-registration

--- Beyond Zenith - frontend/src/Register.jsx
+++ Beyond Zenith - frontend/src/Register.jsx
@@ -11,13 +11,40 @@ function Register(){
         
         <div className="form-wrapper">
           <form>
-            <div className="input-group-reg">
-               <span className="mail-icon">
+            <div className="reg-username">
+               <span className="reg-mail-icon">
+              <ion-icon name="person-add"></ion-icon>
+            </span>
+          <input type="text" required></input>
+          <label>Username :</label>
+          </div>
+            <div className="reg-email">
+               <span className="reg-mail-icon">
               <ion-icon name="mail"></ion-icon>
             </span>
           <input type="email" required></input>
           <label>Email :</label>
               </div>
+              <div className="reg-pass">
+               <span className="reg-pass-icon">
+              <ion-icon name="lock"></ion-icon>
+            </span>
+          <input type="password" required></input>
+          <label>Password :</label>
+          </div>
+              <div className="reg-confirm-pass">
+               <span className="reg-passconf-icon">
+              <ion-icon name="checkmark-circle"></ion-icon>
+            </span>
+          <input type="password" required></input>
+          <label>Confirm Password :</label>
+          </div>
+          <div className="register-button">
+            <button type="submit">Register</button>
+          </div>
+          <div className="register-login">
+            <p>Already have an account? <a href="/login" className="login-link">Login</a></p>
+          </div>
           </form>
         </div>
       </div>
