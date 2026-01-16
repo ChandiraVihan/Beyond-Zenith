4d34c1ed203d57475cf46707fc05fe9c231b7fed
ChandiraVihan <chandiravihan@gmail.com>
2026-01-16 23:42:25 +0530

    apod-grid-layout

--- Beyond Zenith - frontend/src/ApodDetails.jsx
+++ Beyond Zenith - frontend/src/ApodDetails.jsx
@@ -1,13 +1,10 @@
 import './ApodDetails.css'
 import  ApodCard from './ApodCard.jsx'
-import im1 from './assets/im1.jpg'
-import im2 from './assets/im2.jpg'
-import im3 from './assets/im3.jpg'  
 import im4 from './assets/im4.jpg'
 
 function Apod() {
 
-    const testImages = [im1, im2, im3, im4];
+    const testImages = [im4];
 
     const finalImages = testImages.map((img, index) => (
         <ApodCard key={index} testImages={img} />
@@ -15,7 +12,6 @@ function Apod() {
 
     return (
         <div className="apod-view">
-            <h1>APOD Gallery</h1>  
             {finalImages}  
             </div>
     )
