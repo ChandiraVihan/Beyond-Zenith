e3b880dcc40c28d60faec015f9a86a4ee795c4b1
ChandiraVihan <chandiravihan@gmail.com>
2026-01-16 23:01:56 +0530

    direct-image-pass

--- Beyond Zenith - frontend/src/ApodCard.jsx
+++ Beyond Zenith - frontend/src/ApodCard.jsx
@@ -1,8 +1,17 @@
 import './ApodCard.css'
+import im1 from './assets/im1.jpg'
+import im2 from './assets/im2.jpg'
+import im4 from './assets/im4.jpg'
 
  function ApodCard(props) {
+
+  props = {
+    children: [im4]
+  }
+
   return (
     <div className="pic-container">
+    <img src={props.children} alt="Astronomy Picture of the Day" className="apod-image"/>
     </div>
   )
 }
